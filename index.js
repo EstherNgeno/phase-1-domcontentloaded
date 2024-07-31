0968977ed1847de88d25f330f67bc05e60e793dc
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-07-31 16:39:45 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+   
+    const textParagraph = document.getElementById('text');
+    if (textParagraph) {
+        textParagraph.textContent = 'This is really cool!';
+    } else {
+        console.error('Paragraph element with id "text" not found.');
+    }
+});
